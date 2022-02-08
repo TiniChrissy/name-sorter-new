7c2eabce1c86c21482ba203ca7362b57e85ae448
Christina Li <christinal@pageuppeople.com>
2022-02-08 23:15:15 +1100

    renaming

--- Program.cs
+++ Program.cs
@@ -29,72 +29,33 @@ namespace name_sorter
             string[] lines = File.ReadAllLines(path, Encoding.UTF8); //unit test ,if empty return empty, if sorted, return sroted?? probably don't need that
 
             // Get names as List of arrays. Each name is an array: ["Beau", "Tristan", "Bentley"]
-            List<string[]> lastNameFirst = new List<string[]>();
+            List<string[]> names = new List<string[]>();
             var maxNameLength = 0;
             foreach (string line in lines)
             {
                 //Console.WriteLine(line);
                 var fullName = line.Split(' ');
                 var lastName = fullName[fullName.Length - 1];
+                var givenNames = fullName.Take(fullName.Length - 1);
+                string givenNamesOneString = String.Join(" ", givenNames);
 
-
-                var givenames = fullName.Take(fullName.Length - 1);
-                string givenNamesOneString = String.Join(" ", givenames);
-                //Console.WriteLine(givenNamesOneString);
-                var endName = fullName.Skip(fullName.Length - 1);
-                var backwards = endName.Concat(givenames);
-
-                //var forwards = givenNamesOneString.Concat(backwards);
-                //var forwards = lastName.Concat(new[] { givenNamesOneString }).ToArray();
-                //var forwards = [lastName, givenNamesOneString];
                 string[] forwards = new string[] { lastName, givenNamesOneString };
 
                 //Console.WriteLine(forwards);
-                Console.WriteLine(String.Join("+", forwards));
+                //Console.WriteLine(String.Join(" ", forwards));
 
-                lastNameFirst.Add(forwards);
-                //lastNameFirst.Add(backwards);
-
-                // Get maximum name length
-                if (fullName.Length > maxNameLength)
-                {
-                    maxNameLength = fullName.Length;
-                }
-
+                names.Add(forwards);
             }
             // Sort by last name
-            List<string[]> aaa = lastNameFirst
-                .OrderBy(name => name[0])
-                .ThenBy(name => name[1])
+            List<string[]> sortedNames = names
+                .OrderBy(name => name[0])   //name[0]: Last name
+                .ThenBy(name => name[1])    //name[1]: Given name/s
                 .ToList();
 
-
-            //aaa.Sort((a, b) => a[0].CompareTo(b[0]))/*;*/
-
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    for (int j = 0;  j < aaa.Count(); j ++)
-            //    {
-
-            //    }
-
-            //    foreach (string[] name in aaa)
-            //    {
-
-            //    }
-            //    Console.WriteLine(CompareStrings(strFirst, strSecond));
-            //}
-            Console.WriteLine("After sort");
-
-            foreach (string[] name in aaa)
+            // Print to console
+            foreach (string[] name in sortedNames)
             {
                 Console.WriteLine(String.Join(" ", name)); //Print array out as string
-            }
-
-            for (int i = 0; i < maxNameLength; i++)
-            {
-                //Console.WriteLine(i);
-                List<string[]> ddd = lastNameFirst.OrderBy(name => name[name.Length - 1]).ToList();
             }
 
             Boolean CheckIfTxtFile(string fileName)
@@ -112,13 +73,6 @@ namespace name_sorter
                 }
                 Console.WriteLine("This is not a .txt file");
                 return false;
-            }
-
-            //set last name to last word of array
-            // sort entire thing by that index? 
-            object sortArrayOfNames(string[] bbb)
-            {
-                return aaa;
             }
 
         }

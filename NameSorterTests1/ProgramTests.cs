07d3609d734984d362699b34e5dd1e65c9de4c0c
Christina Li <christinal@pageuppeople.com>
2022-02-09 10:35:52 +1100

    moved folder structure up one and added tests

---
+++ NameSorterTests1/ProgramTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NameSorter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void nonTxtFileReturnsFalse()
+        {
+            // Arrange
+            string fileName = "test.doc";
+            // Act
+            bool test = NameSorter.CheckIfTxtFile(fileName);
+            // Assert
+            Assert.IsFalse(test);
+        }
+
+        [TestMethod()]
+        public void txtFileReturnsTrue()
+        {
+            // Arrange
+            string fileName = "test.txt";
+            // Act
+            bool test = NameSorter.CheckIfTxtFile(fileName);
+            // Assert
+            Assert.IsTrue(test);
+        }
+    }
+}
